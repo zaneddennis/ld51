ea975f47ad3b5e46a9ef977f68236b36cc1e75bd
zaneddennis <zaneddennis@gmail.com>
2022-12-30 18:48:46 -0600

    improved main menu visuals

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -1,7 +1,12 @@
 extends CanvasLayer
 
 
+onready var unitsTexture = AtlasTexture.new()
+
+
 func _ready():
+	unitsTexture.atlas = load("res://ActiveGame/Art/Tiles/Units.png")
+	
 	$AudioStreamPlayer.play()
 
 
@@ -11,3 +16,15 @@ func _on_StartGame_pressed():
 
 func _on_ExitToDesktop_pressed():
 	get_tree().quit()
+
+
+func _on_FlipTimer_timeout():	
+	var i = 1 + randi()%16
+	var tr = get_node("Background/Content/GridContainer/Card%d/Unit" % i)
+	
+	var newUnitName = Util.Choice(Database.cards.keys())
+	var textureLoc = Database.cards[newUnitName]["texture"] * 64
+	
+	print(newUnitName)
+	tr.texture = unitsTexture.duplicate()
+	tr.texture.region = Rect2(textureLoc, Vector2(64, 64))
